46cc0e284e6049d8009471678a176f8d73d43b38
hazem.saleh <hazem.saleh@ee601984-c251-0410-a2d3-c3ca3d9b805d>
2009-05-29 02:05:01 +0000

    made a copy

---
+++ branches/1.1.1/examples/src/main/java/com/googlecode/gmaps4jsf/example/beans/Tree2Constants.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.googlecode.gmaps4jsf.example.beans;
+
+public interface Tree2Constants {
+    public static final String WORLD_FACET_NAME = "world";
+    public static final String CONTINENT_FACET_NAME = "continent";
+    public static final String COUNTRY_FACET_NAME = "country";
+    public static final String CONTINENTS_ID = "continents";
+    public static final String CONTINENT_Africa_ID = "africa";
+    public static final String CONTINENT_Asia_ID = "asia";
+    public static final String CONTINENT_Europe_ID = "europe";
+    public static final String CONTINENT_America_ID = "america";
+    public static final String CONTINENT_North_America_ID = "north america";
+    public static final String CONTINENT_South_America_ID = "south america";
+}
