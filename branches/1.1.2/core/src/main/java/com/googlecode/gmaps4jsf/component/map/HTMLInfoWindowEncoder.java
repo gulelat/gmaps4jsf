bcdd78a98d9dbc36c10bae424c957cfa6283c05a
hazem.saleh <hazem.saleh@ee601984-c251-0410-a2d3-c3ca3d9b805d>
2009-07-21 20:49:10 +0000

    GMaps4JSF 1.1.2 release.

---
+++ branches/1.1.2/core/src/main/java/com/googlecode/gmaps4jsf/component/map/HTMLInfoWindowEncoder.java
@@ -0,0 +1,149 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.googlecode.gmaps4jsf.component.map;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+
+import com.googlecode.gmaps4jsf.component.eventlistener.EventListener;
+import com.googlecode.gmaps4jsf.component.htmlInformationWindow.HTMLInformationWindow;
+import com.googlecode.gmaps4jsf.component.marker.Marker;
+import com.googlecode.gmaps4jsf.util.ComponentConstants;
+
+/**
+ * @author Hazem Saleh
+ * @date Jul 31, 2008
+ * The HTMLInfoWindowEncoder is used for encpding the map info windows.
+ */
+public class HTMLInfoWindowEncoder {
+    
+	/**
+	 * The (encodeMarkerHTMLInfoWindow) method is used for encoding the 
+	 * HTMLInfoWindow when its parent is the marker. 
+	 * @param facesContext
+	 * @param marker
+	 * @param window
+	 * @param writer
+	 * @throws IOException
+	 */
+	public static void encodeMarkerHTMLInfoWindow(FacesContext facesContext,
+			Marker marker, HTMLInformationWindow window, ResponseWriter writer)
+			throws IOException {
+
+		writer.write(ComponentConstants.CONST_MARKER_PREFIX + marker.getId()
+				+ ".openInfoWindowHtml('" + window.getHtmlText() + "');    ");
+
+		writer.write("var window_" + window.getId() + " = "
+				+ ComponentConstants.JS_GMAP_BASE_VARIABLE
+				+ ".getInfoWindow();    ");
+
+		// encode window events.
+		for (Iterator iterator = window.getChildren().iterator(); iterator
+				.hasNext();) {
+			UIComponent component = (UIComponent) iterator.next();
+
+			if (component instanceof EventListener) {
+				EventEncoder.encodeEventListenersFunctionScript(facesContext,
+						window, writer, "window_" + window.getId());
+				EventEncoder.encodeEventListenersFunctionScriptCall(
+						facesContext, window, writer, "window_"
+								+ window.getId());
+			}
+		}
+	}	
+	
+	public static void encodeHTMLInfoWindowsFunctionScript(FacesContext facesContext,
+			Map mapComponent, ResponseWriter writer) throws IOException {
+
+		writer.write("function "
+				+ ComponentConstants.JS_CREATE_HTMLINFOWINDOWS_FUNCTION_PREFIX
+				+ mapComponent.getId() + "("
+				+ ComponentConstants.JS_GMAP_BASE_VARIABLE + ") {");
+		for (Iterator iterator = mapComponent.getChildren().iterator(); iterator
+				.hasNext();) {
+			UIComponent component = (UIComponent) iterator.next();
+
+			if (component instanceof HTMLInformationWindow && component.isRendered()) {
+				encodeMapHTMLInfoWindow(facesContext, mapComponent,
+						(HTMLInformationWindow) component, writer);
+			}
+		}
+		writer.write("}");
+	}	
+	
+	public static void encodeHTMLInfoWindowsFunctionScriptCall(
+			FacesContext facesContext, Map mapComponent, ResponseWriter writer)
+			throws IOException {
+
+		writer.write(ComponentConstants.JS_CREATE_HTMLINFOWINDOWS_FUNCTION_PREFIX
+						+ mapComponent.getId()
+						+ "("
+						+ ComponentConstants.JS_GMAP_BASE_VARIABLE + ");    ");
+	}
+    
+    private static void encodeMapHTMLInfoWindow(FacesContext facesContext,
+            Map mapComponent, HTMLInformationWindow window,
+            ResponseWriter writer) throws IOException {
+
+        String longitude;
+        String latitude;
+
+        if (window.getLatitude() != null) {
+            latitude = window.getLatitude();
+        } else {
+            latitude = ComponentConstants.JS_GMAP_BASE_VARIABLE
+                    + ".getCenter().lat()";
+        }
+
+        if (window.getLongitude() != null) {
+            longitude = window.getLongitude();
+        } else {
+            longitude = ComponentConstants.JS_GMAP_BASE_VARIABLE
+                    + ".getCenter().lng()";
+        }
+
+        writer.write(ComponentConstants.JS_GMAP_BASE_VARIABLE
+                + ".openInfoWindowHtml(new "
+                + ComponentConstants.JS_GLatLng_OBJECT + "(" + latitude + ", "
+                + longitude + "), '" + window.getHtmlText() + "');    ");   
+        
+        writer.write("var window_" + window.getId() + " = "
+                + ComponentConstants.JS_GMAP_BASE_VARIABLE
+                + ".getInfoWindow();    "); 
+    
+        // encode window events.
+        for (Iterator iterator = window.getChildren().iterator(); iterator
+                .hasNext();) {
+            UIComponent component = (UIComponent) iterator.next();
+
+            if (component instanceof EventListener) {
+                EventEncoder.encodeEventListenersFunctionScript(facesContext,
+                        window, writer, "window_"
+                                + window.getId());
+                EventEncoder.encodeEventListenersFunctionScriptCall(
+                        facesContext, window, writer, "window_"
+                                + window.getId());
+            }
+        }           
+    }    
+}
---
+++ branches/1.1.2/examples/src/main/webapp/pages/jsVariableExample.jsp
@@ -0,0 +1,49 @@
+<%@ taglib uri="http://java.sun.com/jsf/html" prefix="h" %>
+<%@ taglib uri="http://java.sun.com/jsf/core" prefix="f" %>
+<%@ taglib uri="http://code.google.com/p/gmaps4jsf/" prefix="m" %>
+<!DOCTYPE html "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<HTML xmlns="http://www.w3.org/1999/xhtml">
+    <HEAD> 
+    <title>Welcome to GMaps4JSF</title> 
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />    	
+    <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAAxrVS1QxlpJHXxQ2Vxg2bJBQdkFK-tWRbPPQS4ACM1pq_e-PltxQXeyH20wQuqDaQ_6EM5UeGGVpnIw"
+      type="text/javascript"></script>
+  
+    </HEAD>
+	
+	<body onunload="GUnload()">
+	<f:view>
+    	<h:form id="form">
+    		<m:map width="500px" height="500px" latitude="30.01" longitude="31.14" jsVariable="map1">
+    			<m:marker latitude="30.01" longitude="31.14" jsVariable="marker1"/>
+    			<m:htmlInformationWindow latitude="30.01" longitude="31.14" htmlText="Egypt"/>
+    		</m:map>
+    		<br/>
+    		<input type="button" value="Zoom In" onclick="zoomIn();">
+    		<input type="button" value="Zoom Out" onclick="zoomOut();"> <br/>
+    		<input type="button" value="Hide Marker" onclick="hideMarker();">
+    		<input type="button" value="Show Marker" onclick="showMarker();"><br/>			
+    		<script>
+    			function zoomIn() {
+    				map1.zoomIn();
+    			}
+  
+    			function zoomOut() {
+    				map1.zoomOut();
+    			}  
+  
+    			function hideMarker() {
+    				marker1.hide();
+    			}  
+    			
+    			function showMarker() {
+    				marker1.show();
+    			}      			    			  			  			
+    		</script>
+    		
+    	</h:form>
+	</f:view>
+	<%@include file="../templates/footer.jspf" %>   	
+    </body>
+</HTML>  
