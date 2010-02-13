73475ac41d1c826037e1ddd04dd2e779d44aa692
jose.noheda <jose.noheda@ee601984-c251-0410-a2d3-c3ca3d9b805d>
2010-02-13 11:54:14 +0000

    Fixes last commit (missing files). Upgrades drag zoom component to the new JS approach

--- plugins/gmaps-utility-library-dev/marker-clusterer/src/main/java/com/googlecode/gmaps4jsf/plugins/gmapsutility/cluster/MarkerClusterEncoder.java
+++ plugins/gmaps-utility-library-dev/marker-clusterer/src/main/java/com/googlecode/gmaps4jsf/plugins/gmapsutility/cluster/MarkerClusterEncoder.java
@@ -16,50 +16,40 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package com.googlecode.gmaps4jsf.plugins.gmapsutility.dragzoom;
+package com.googlecode.gmaps4jsf.plugins.gmapsutility.cluster;
 
 import java.util.Iterator;
-
-import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
-
+import javax.faces.component.UIComponent;
 import com.googlecode.gmaps4jsf.plugins.MapPlugin;
 import com.googlecode.gmaps4jsf.util.ComponentConstants;
-import com.googlecode.gmaps4jsf.plugins.gmapsutility.component.DragZoomControl;
+import com.googlecode.gmaps4jsf.plugins.gmapsutility.component.MarkerCluster;
 
 /**
+ * Encodes the JS needed to group markers automatically when they reach a threshold.
  *
  * @author Jose Noheda [jose.noheda@gmail.com]
  */
-public final class DragZoomControlEncoder extends MapPlugin {
+public final class MarkerClusterEncoder extends MapPlugin {
 
-    private static final String DRAG_ZOOM_FUNCTION = "dragZoom";
+    private static final String MARKER_CLUSTERER_FUNCTION = "markerClusterer";
 
-    public String encodeFunctionScript(FacesContext facesContext, UIComponent mapComponent) {
+    public String encodeFunctionScript(FacesContext facesContext, UIComponent clusterComponent) {
         StringBuffer buffer = new StringBuffer("function ");
-        buffer.append(DRAG_ZOOM_FUNCTION).append(mapComponent.getId()).append("(map) {");
-        for (Iterator iterator = mapComponent.getChildren().iterator(); iterator.hasNext();) {
+        buffer.append(MARKER_CLUSTERER_FUNCTION).append(clusterComponent.getId()).append("(map) {");
+        for (Iterator iterator = clusterComponent.getChildren().iterator(); iterator.hasNext();) {
             UIComponent component = (UIComponent) iterator.next();
-            if (component instanceof DragZoomControl  && component.isRendered()) {
-                encodeDragZoomControl((DragZoomControl) component, buffer);
+            if (component instanceof MarkerCluster  && component.isRendered()) {
+                buffer.append("map.setCluster(new MarkerClusterer(map));");
             }
         }
         return buffer.append("}").toString();
     }
 
-    public String encodeFunctionScriptCall(FacesContext facesContext, UIComponent mapComponent) {
+    public String encodeFunctionScriptCall(FacesContext facesContext, UIComponent clusterComponent) {
         StringBuffer buffer = new StringBuffer();
-        return buffer.append(DRAG_ZOOM_FUNCTION).append(mapComponent.getId())
+        return buffer.append(MARKER_CLUSTERER_FUNCTION).append(clusterComponent.getId())
             .append("(").append(ComponentConstants.JS_GMAP_BASE_VARIABLE).append(");").toString();
-    }
-
-    private void encodeDragZoomControl(DragZoomControl zoomControl, StringBuffer buffer) {
-        buffer.append(ComponentConstants.JS_GMAP_BASE_VARIABLE).append(".")
-            .append(DRAG_ZOOM_FUNCTION)
-            .append("('").append(zoomControl.getImageURL())
-            .append("', '").append(zoomControl.getImageZoomingURL())
-            .append("', '").append(zoomControl.getImageWidth())
-            .append("');");
     }
 
 }
